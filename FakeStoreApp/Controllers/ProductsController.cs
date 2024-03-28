471ec072ee1bec26234d342697f1ae9f1678d27d
ibrahimfahfouhi <i.fahfouhi.pfe@b3gtech.com>
2024-03-28 01:13:39 +0000

    now, the ProductsController is completed

--- FakeStoreApp/Controllers/ProductsController.cs
+++ FakeStoreApp/Controllers/ProductsController.cs
@@ -12,6 +12,20 @@ namespace FakeStoreApp.Controllers
         {
             _dbContext = fakeStoreDbContext;
         }
+        [HttpGet]
+        [Route("")]
+        public ActionResult<IEnumerable<Product>> getAllProducts() { 
+           var products = _dbContext.Set<Product>().ToList();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetProductById(int id) {
+            var product = _dbContext.Set<Product>().Find(id);
+            return product == null? NotFound() : Ok(product); 
+        }
+
         [HttpPost]
         [Route("")]
         public ActionResult<int> creatProduct(Product product) {
@@ -19,6 +33,27 @@ namespace FakeStoreApp.Controllers
             _dbContext.Set<Product>().Add(product);
             _dbContext.SaveChanges();
             return Ok(product);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public ActionResult updateProduct(Product product)
+        {
+            var existingProduct = _dbContext.Set<Product>().Find(product.id);
+            existingProduct.name = product.name;
+            existingProduct.sku = product.sku;
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult deleteProduct(int id)
+        {
+            var productToDelete = _dbContext.Set<Product>().Find(id);
+            _dbContext.Set<Product>().Remove(productToDelete);
+            _dbContext.SaveChanges();
+            return Ok();
         }
     }
 }
